bf028ed2a8dcf96894a02574153516d9f4ba5848
Samrat Ganguly <81913029+samrat-ganguly@users.noreply.github.com>
2022-08-04 17:52:53 +0530

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,47 @@
+function ClearFields1(){
+    document.getElementById("text1").value = "";
+}
+function ClearFields2(){
+    document.getElementById("text2").value = "";
+}
+function ClearFields3(){
+    document.getElementById("text3").value = "";
+}
+function ClearFields4(){
+    document.getElementById("text4").value = "";
+}
+function ClearFields5(){
+    document.getElementById("text5").value = "";
+}
+function ClearFields6(){
+    document.getElementById("text6").value = "";
+}
+
+function submit1(){
+    var input = document.getElementById("test1");
+    if(input==null)
+    alert("No Task Available");
+    else
+    alert("Task Saved");
+}
+function submit2(){
+    var input = document.getElementById("test1");
+    alert("Task Saved");
+}
+function submit3(){
+    var input = document.getElementById("test1");
+    alert("Task Saved");
+}
+function submit4(){
+    var input = document.getElementById("test1");
+    alert("Task Saved");
+}
+function submit5(){
+    var input = document.getElementById("test1");
+    alert("Task Saved"); 
+}
+function submit6(){
+    var input = document.getElementById("test1");
+    alert("Task Saved");
+}
+
